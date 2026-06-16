72f3bce5f25843e8d72826c6baff89474d650d93
Kyale33 <ronchi.kyara@pucp.edu.pe>
2026-06-16 00:29:04 -0500

    Unimos las bases de datos de los módulos 100, 300, 400 y 800 de tal forma de que nuestra unidad de análisis o un caso sea el informante del hogar

--- scripts/02_UnionBases_190626.R
+++ scripts/02_UnionBases_190626.R
@@ -10,6 +10,7 @@ library(rio)
 library(tidyverse)
 library(janitor)
 library(readr)
+library(dplyr)
 
 # 2. Se usa renv-------------------------------------------------------------
 # Se utiliza renv::init() para guardar la versión actual de las librerías utilizadas
@@ -27,3 +28,28 @@ mod300  = import("Enaho01A-2025-300.csv", encoding = "Latin-1")
 mod400  = import("Enaho01A-2025-400.csv", encoding = "Latin-1")
 mod_pc1 = import("Enaho01-2025-800A.csv", encoding = "Latin-1")
 mod_pc2 = import("Enaho01-2025-800B.csv", encoding = "Latin-1")
+
+# 4. Unión de datos-------------------------------------------------------
+# El key de hogar se estructura para darle un identificador al hogar encuestado
+keys_hogar <- c("AÑO", "MES", "CONGLOME", "VIVIENDA", "HOGAR")
+# El key de persona se estructura para darle un identificador a la persona encuestada
+keys_personas <- c(keys_hogar, "CODPERSO")
+
+# Unimos los módulos 300 (educación) y 400 (salud) por las variables comunes de keys_personas, ya que las preguntas de ambos módulos son para todas las personas y no para una sola por hogar
+base_x_persona <- mod400 %>%
+  left_join(mod300, by = keys_personas)
+
+# Unimos el módulo 800 (participación ciudadana) adaptando la key que falta con otras variables que hay en común, ya que no necesariamente todos los que respondieron el módulo 300 y 400 también respondieron el 800 (Que suele ser sólo para el informante del hogar)
+base_personas <- base_x_persona %>%
+  left_join(mod_pc1, by = c(
+    "AÑO" = "AÑO", 
+    "MES" = "MES", 
+    "CONGLOME" = "CONGLOME", 
+    "VIVIENDA" = "VIVIENDA", 
+    "HOGAR" = "HOGAR", 
+    "CODPERSO" = "CODINFOR"
+  ))
+
+# Unimos las bases de datos para que podamos tener como unidad de análisis al informante del hogar
+base_final <- base_personas %>%
+  left_join(mod100, by = keys_hogar)
