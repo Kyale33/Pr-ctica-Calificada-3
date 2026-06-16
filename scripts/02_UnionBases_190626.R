cf717e08a4d689d5b3f8a091a07662e740cbf213
Kyale33 <ronchi.kyara@pucp.edu.pe>
2026-06-15 23:58:22 -0500

    Subimos las 5 bases de datos importantes para el proyecto: los módulos de vivienda (100), educación (300), salud (400) y participación ciudanana (800A y 800B)

--- scripts/02_UnionBases_190626.R
+++ scripts/02_UnionBases_190626.R
@@ -14,3 +14,16 @@ library(readr)
 # 2. Se usa renv-------------------------------------------------------------
 # Se utiliza renv::init() para guardar la versión actual de las librerías utilizadas
 renv::snapshot(force = TRUE)
+
+# 3. Importar datos------------------------------------------------------
+# Se cargan los módulos de vivienda, educación, salud y participación ciudadana para el análisis
+# - Módulo 100: Vivienda
+# - Módulo 300: Educación
+# - Módulo 400: Salud
+# - Módulo 800: Participación Ciudadana
+
+mod100  = import("Enaho01-2025-100.csv", encoding = "Latin-1")
+mod300  = import("Enaho01A-2025-300.csv", encoding = "Latin-1")
+mod400  = import("Enaho01A-2025-400.csv", encoding = "Latin-1")
+mod_pc1 = import("Enaho01-2025-800A.csv", encoding = "Latin-1")
+mod_pc2 = import("Enaho01-2025-800B.csv", encoding = "Latin-1")
